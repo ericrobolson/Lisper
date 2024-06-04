0c5c81a061eca9694e6d4fd556d3d1f1a57a0e1d
Eric Olson <eric.rob.olson@gmail.com>
2024-06-04 15:20:01 -0700

    Added in error casting

--- src/error.rs
+++ src/error.rs
@@ -5,3 +5,18 @@ pub struct Error<Kind> {
     pub kind: Kind,
     pub location: Location,
 }
+
+impl ToString for Error<String> {
+    fn to_string(&self) -> String {
+        let loc_error = match self.location.path {
+            Some(ref path) => format!(
+                "{}:{}:{}",
+                path.display(),
+                self.location.line,
+                self.location.column
+            ),
+            None => format!("{}:{}", self.location.line, self.location.column),
+        };
+        format!("{}: {}", loc_error, self.kind)
+    }
+}
